666b9f155d6559441b7e0aa40209988cca6f71e7
jenny <kjn3008@gmail.com>
2023-03-22 14:06:42 +0900

    session04

---
+++ main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog
+from django.utils import timezone
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request, 'main/detail.html',{'blog':blog})
+
+def mainpage(request):
+    blogs = Blog.objects.all()
+    return render(request, 'main/mainpage.html', {'blogs':blogs})
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.pub_date = timezone.now()
+    new_blog.body = request.POST['body']
+
+    new_blog.save()
+    
+    return redirect('detail', new_blog.id)
+
+def new(request):
+    return render(request, 'main/new.html')
